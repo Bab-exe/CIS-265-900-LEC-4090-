bd620aa152ee345d23557779809514145938e4ae
Blessing <96212794+Bab-exe@users.noreply.github.com>
2024-02-28 18:39:51 -0500

    Lab 5 released
    Lab 5.2 finished as of rn

---
+++ CIS 265 - Labs/CIS 265 - Lab 5/Lab 5/Lab 5.2/Lab 5.2/Person.vb
@@ -0,0 +1,23 @@
+﻿'question 1 
+Public Class Person
+
+    Protected _first, _last As String
+
+    Protected _birthday As DateTime
+
+    Public Sub New(first As String, last As String, birth As DateTime)
+        _first = first
+        _last = last
+        Me._birthday = birth
+    End Sub
+
+    Public Overrides Function ToString() As String
+
+        Return $"First name: {_first}
+Last name: {_last}
+Birthday: {_birthday}
+"
+    End Function
+
+
+End Class
---
+++ CIS 265 - Labs/CIS 265 - Lab 5/Lab 5/Lab 5.2/Lab 5.2/Program.vb
@@ -0,0 +1,75 @@
+Imports System
+'Blessing Abumere CIS 265 Lab 5' 
+
+
+Module Program
+
+    'question 3?'
+    Public Function Input_Student() As Student
+        Dim first_name, last_name As String
+        Dim birthday As DateTime
+
+        Dim homeroom As Integer
+        Dim grade As String
+        Dim gpa As Double
+        Dim graduation As DateTime
+
+        Console.WriteLine("Enter the student's first name: ")
+        first_name = Console.ReadLine()
+
+        Console.WriteLine("Enter the student's last name: ")
+        last_name = Console.ReadLine()
+
+        Console.WriteLine("Enter the student's birthday: ")
+        birthday = Console.ReadLine()
+
+        Console.WriteLine("Enter the student's homeroom number: ")
+        homeroom = Console.ReadLine()
+
+        Console.WriteLine("Enter the student's grade year: ")
+        grade = Console.ReadLine()
+
+        Console.WriteLine("Enter the student's gpa: ")
+        gpa = Console.ReadLine()
+
+        Console.WriteLine("Enter the student's graduation day: ")
+        graduation = Console.ReadLine()
+
+        Return New Student(first_name, last_name, birthday, homeroom, grade, gpa, graduation)
+    End Function
+
+
+    'question 4 '
+    Public Sub GPA_Filter(Students As List(Of Student), Optional Filter As Double = 3.0)
+        Dim output As String = ""
+
+        For Each student In Students
+            If student.GPA() >= Filter Then
+                output &= $"{student.ToString()} {Environment.NewLine}"
+            End If
+        Next
+
+        If output.Equals("") Then
+            output = "None"
+        End If
+
+        Console.WriteLine($"{Environment.NewLine}Students with GPA's greater than or equal to {Filter}: {Environment.NewLine}{output}")
+
+    End Sub
+
+    Sub Main(args As String())
+
+        Dim Students As New List(Of Student)
+
+        'question 3'
+        For i As Integer = 1 To 5
+            Students.Add(Input_Student())
+        Next
+
+        'question 4'
+        GPA_Filter(Students)
+
+        'todo check if protected or private changes anything for person.vb 
+
+    End Sub
+End Module
