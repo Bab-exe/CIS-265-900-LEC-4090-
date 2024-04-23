44d1391c83efb6f9873ef634ed7300b7a90f3f62
Blessing <babumere04@gmail.com>
2024-04-22 23:56:25 -0400

    lab 10 completed

---
+++ CIS 265 - Labs/CIS 265 - Lab 10/Lab10/Form1.Designer.vb
@@ -0,0 +1,164 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btn_BrowseDB = New System.Windows.Forms.Button()
+        Me.txt_DBDir = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txt_DataFileDir = New System.Windows.Forms.TextBox()
+        Me.btn_BrowseDataFile = New System.Windows.Forms.Button()
+        Me.btn_LoadData = New System.Windows.Forms.Button()
+        Me.Listbox_Data = New System.Windows.Forms.ListBox()
+        Me.btn_FindSeniors = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.ToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'btn_BrowseDB
+        '
+        Me.btn_BrowseDB.Location = New System.Drawing.Point(510, 18)
+        Me.btn_BrowseDB.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btn_BrowseDB.Name = "btn_BrowseDB"
+        Me.btn_BrowseDB.Size = New System.Drawing.Size(36, 35)
+        Me.btn_BrowseDB.TabIndex = 1
+        Me.btn_BrowseDB.Text = "..."
+        Me.btn_BrowseDB.UseVisualStyleBackColor = True
+        '
+        'txt_DBDir
+        '
+        Me.txt_DBDir.Location = New System.Drawing.Point(111, 22)
+        Me.txt_DBDir.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txt_DBDir.Name = "txt_DBDir"
+        Me.txt_DBDir.Size = New System.Drawing.Size(388, 26)
+        Me.txt_DBDir.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(18, 26)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 20)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Database:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(24, 109)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Data File:"
+        '
+        'txt_DataFileDir
+        '
+        Me.txt_DataFileDir.Location = New System.Drawing.Point(111, 105)
+        Me.txt_DataFileDir.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txt_DataFileDir.Name = "txt_DataFileDir"
+        Me.txt_DataFileDir.Size = New System.Drawing.Size(388, 26)
+        Me.txt_DataFileDir.TabIndex = 5
+        '
+        'btn_BrowseDataFile
+        '
+        Me.btn_BrowseDataFile.Location = New System.Drawing.Point(510, 102)
+        Me.btn_BrowseDataFile.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btn_BrowseDataFile.Name = "btn_BrowseDataFile"
+        Me.btn_BrowseDataFile.Size = New System.Drawing.Size(36, 35)
+        Me.btn_BrowseDataFile.TabIndex = 1
+        Me.btn_BrowseDataFile.Text = "..."
+        Me.btn_BrowseDataFile.UseVisualStyleBackColor = True
+        '
+        'btn_LoadData
+        '
+        Me.btn_LoadData.Location = New System.Drawing.Point(111, 145)
+        Me.btn_LoadData.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btn_LoadData.Name = "btn_LoadData"
+        Me.btn_LoadData.Size = New System.Drawing.Size(112, 35)
+        Me.btn_LoadData.TabIndex = 6
+        Me.btn_LoadData.Text = "Load Data"
+        Me.btn_LoadData.UseVisualStyleBackColor = True
+        Me.btn_LoadData.Visible = False
+        '
+        'Listbox_Data
+        '
+        Me.Listbox_Data.FormattingEnabled = True
+        Me.Listbox_Data.HorizontalScrollbar = True
+        Me.Listbox_Data.ItemHeight = 20
+        Me.Listbox_Data.Location = New System.Drawing.Point(28, 199)
+        Me.Listbox_Data.MultiColumn = True
+        Me.Listbox_Data.Name = "Listbox_Data"
+        Me.Listbox_Data.ScrollAlwaysVisible = True
+        Me.Listbox_Data.Size = New System.Drawing.Size(518, 124)
+        Me.Listbox_Data.TabIndex = 7
+        Me.Listbox_Data.Visible = False
+        '
+        'btn_FindSeniors
+        '
+        Me.btn_FindSeniors.Enabled = False
+        Me.btn_FindSeniors.Location = New System.Drawing.Point(434, 145)
+        Me.btn_FindSeniors.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btn_FindSeniors.Name = "btn_FindSeniors"
+        Me.btn_FindSeniors.Size = New System.Drawing.Size(112, 35)
+        Me.btn_FindSeniors.TabIndex = 9
+        Me.btn_FindSeniors.Text = "Find Seniors"
+        Me.btn_FindSeniors.UseVisualStyleBackColor = True
+        Me.btn_FindSeniors.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(578, 344)
+        Me.Controls.Add(Me.btn_FindSeniors)
+        Me.Controls.Add(Me.Listbox_Data)
+        Me.Controls.Add(Me.btn_LoadData)
+        Me.Controls.Add(Me.txt_DataFileDir)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt_DBDir)
+        Me.Controls.Add(Me.btn_BrowseDataFile)
+        Me.Controls.Add(Me.btn_BrowseDB)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "Form1"
+        Me.Text = "Lab10"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_BrowseDB As Button
+    Friend WithEvents txt_DBDir As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txt_DataFileDir As TextBox
+    Friend WithEvents btn_BrowseDataFile As Button
+    Friend WithEvents btn_LoadData As Button
+    Friend WithEvents Listbox_Data As ListBox
+    Friend WithEvents btn_FindSeniors As Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents ToolTip As ToolTip
+End Class
---
+++ CIS 265 - Labs/CIS 265 - Lab 10/Lab10/Form1.vb
@@ -0,0 +1,152 @@
+﻿Imports System.IO
+Imports System.Data.OleDb
+
+Public Class Form1
+
+    Dim con As New OleDb.OleDbConnection
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SetDefaultVisual(txt_DBDir)
+        SetDefaultVisual(txt_DataFileDir)
+    End Sub
+
+    Private Sub SetDefaultVisual(textBox As System.Windows.Forms.TextBox)
+        textBox.ReadOnly = True
+        textBox.Text = "Browse for a file"
+        textBox.Font = New Font(textBox.Font, FontStyle.Italic)
+        textBox.ForeColor = Color.Gray
+    End Sub
+    Private Sub btn_BrowseDB_Click(sender As Object, e As EventArgs) Handles btn_BrowseDB.Click
+
+        Dim ofd As New OpenFileDialog()
+        ofd.Title = "Select MDB or ACCDB File"
+        ofd.Filter = "Access Database Files (*.mdb, *.accdb)|*.mdb;*.accdb"
+        ofd.Multiselect = False
+        ofd.InitialDirectory = Environment.CurrentDirectory
+
+        If ofd.ShowDialog() = DialogResult.OK Then
+
+            ' Retrieve the selected file path
+            Dim selectedFilePath As String = ofd.FileName
+
+            ' Now you can use the selected file path as needed
+            txt_DBDir.Font = New Font(txt_DBDir.Font, FontStyle.Regular)
+            txt_DBDir.ForeColor = Color.Black
+            txt_DBDir.Text = selectedFilePath
+            ' Move the cursor to the end of the text
+            txt_DBDir.SelectionStart = txt_DBDir.Text.Length
+
+            SetupDBCon(selectedFilePath)
+
+            '1.	The Load Data button should be invisible until after the user has selected a database file and has selected the database."
+            btn_LoadData.Visible = True
+
+        End If
+
+    End Sub
+
+    Private Sub SetupDBCon(dbPath As String)
+
+        Dim conStr As String = "Data Source=lab10.mdb; Provider=Microsoft.Jet.OLEDB.4.0;" '32bit
+        'Dim conStr As String = "Data Source=lab10.accdb; Provider=Microsoft.ACE.OLEDB.12.0;" '64bit
+
+        con = New OleDb.OleDbConnection(conStr)
+
+    End Sub
+
+
+
+
+    Private Sub btn_BrowseDataFile_Click(sender As Object, e As EventArgs) Handles btn_BrowseDataFile.Click
+
+        Dim ofd As New OpenFileDialog()
+        ofd.Title = "Select a Text File"
+        ofd.Filter = "Text Files (*.txt)|*.txt"
+        ofd.Multiselect = False
+        ofd.InitialDirectory = Environment.CurrentDirectory
+
+        If ofd.ShowDialog() = DialogResult.OK Then
+            Dim selectedFilePath As String = ofd.FileName
+            txt_DataFileDir.Font = New Font(txt_DBDir.Font, FontStyle.Regular)
+            txt_DataFileDir.ForeColor = Color.Black
+            txt_DataFileDir.Text = selectedFilePath
+            txt_DataFileDir.SelectionStart = txt_DBDir.Text.Length
+        End If
+
+    End Sub
+
+    Private Sub btn_LoadData_Click(sender As Object, e As EventArgs) Handles btn_LoadData.Click
+        'Import the contents of the file into the database
+
+        Dim filePath As String = txt_DataFileDir.Text
+
+        Using connection As New OleDbConnection(con.ConnectionString)
+
+            con.Open()
+
+            ' Read the flat file line by line
+            For Each line As String In File.ReadLines(filePath)
+
+                ' Split the line into individual values based on commas
+                'This is saved in the data array
+                Dim data As String() = line.Split(","c)
+
+                ' SQL command to insert data into the table
+                Dim sql As String = "INSERT INTO Student (FirstName, LastName, GradeLevel, GPA) " &
+                                    $"VALUES ('{data(0)}', '{data(1)}', {data(2)}, {data(3)})"
+
+                Using cmd As New OleDbCommand(sql, con)
+                    cmd.ExecuteNonQuery()
+
+                End Using
+
+            Next
+
+            con.Close()
+        End Using
+
+
+
+
+
+        '2.	Display a message box to the user after the data has been successfully imported into the Student table.
+        MsgBox("data has been successfully imported into the Student table.", MsgBoxStyle.Information, "Data Imported")
+
+
+
+
+        '6.	Find Seniors button should not be clickable until after data has been imported into the Student table (Load Data has been clicked).
+        btn_FindSeniors.Visible = True
+        btn_FindSeniors.Enabled = True
+
+
+
+    End Sub
+
+    Private Sub btn_FindSeniors_Click(sender As Object, e As EventArgs) Handles btn_FindSeniors.Click
+
+        Listbox_Data.Visible = True
+        Listbox_Data.Items.Clear()
+
+
+        '4.	Query the Student table to find all 12th graders. Add their StudentIDs into the ListBox.
+        Using connection As New OleDbConnection(con.ConnectionString)
+
+            con.Open()
+
+            Dim sql As String = "SELECT StudentID FROM Student WHERE GradeLevel = 12"
+
+            Using cmd As New OleDbCommand(sql, con)
+                Dim reader As OleDbDataReader = cmd.ExecuteReader()
+
+                While reader.Read()
+                    Listbox_Data.Items.Add(reader("StudentID"))
+                End While
+
+            End Using
+
+            con.Close()
+        End Using
+
+        ToolTip.Show($"{Listbox_Data.Items.Count} Senior IDS found.", btn_FindSeniors)
+    End Sub
+End Class
